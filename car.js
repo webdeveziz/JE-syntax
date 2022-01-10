698b7675b5b53ccc9682b94668d7d342af026e5f
webdeveziz <38165397+webdeveziz@users.noreply.github.com>
2022-01-11 01:23:20 +0300

    Add files via upload

---
+++ car.js
@@ -0,0 +1,21 @@
+const carName = 'Мерседес'
+alert(carName)
+let maxCarSpeed = '316 км/ч'
+alert(maxCarSpeed)
+let carOwner = 'Eziz'
+alert(carOwner)
+
+maxCarSpeed = '310 км/ч'
+carOwner = 'Ivan'
+console.log('У машины изменилась скорость: ', maxCarSpeed)
+console.log('У машины изменился владелец: ', carOwner)
+
+maxCarSpeed = '309 км/ч'
+carOwner = 'Andrey'
+console.log('У машины изменилась скорость: ', maxCarSpeed)
+console.log('У машины изменился владелец: ', carOwner)
+
+maxCarSpeed = '299 км/ч'
+carOwner = 'John'
+console.log('У машины изменилась скорость: ', maxCarSpeed)
+console.log('У машины изменился владелец: ', carOwner)
---
+++ index.js
@@ -0,0 +1,17 @@
+//JavaScript код с созданием переменных
+
+/* 
+Автор:
+Курбанназаров Эзиз
+*/
+
+const myName = 'Eziz'
+console.log('🚀 ~ myName: ', myName)
+const mySurname = 'Kurbannazarov'
+console.log('🚀 ~ mySurname: ', mySurname)
+let myFavoriteDrink = 'Coca-cola'
+console.log('🚀 ~ myFavoriteDrink: ', myFavoriteDrink)
+let myFavoriteAnimal = 'Собака'
+console.log('🚀 ~ myFavoriteAnimal: ', myFavoriteAnimal)
+const myFavoriteProgrammingLanguage = 'JavaScript'
+console.log('🚀 ~ myFavoriteLanguage: ', myFavoriteProgrammingLanguage)
